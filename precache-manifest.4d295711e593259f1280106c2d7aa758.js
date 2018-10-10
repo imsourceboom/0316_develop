5c201a36c17bc8d9f520116f8aecc9f578728b94
imsourceboom <ecmascript.developer@gmail.com>
2018-10-10 20:47:23 +0900

    Updates

---
+++ precache-manifest.4d295711e593259f1280106c2d7aa758.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e8b4cdf9f7ca6ebd681",
+    "url": "/0316_develop/static/js/runtime~main.3e8b4cdf.js"
+  },
+  {
+    "revision": "8673fdf2885ab0e3f8f2",
+    "url": "/0316_develop/static/js/main.8673fdf2.chunk.js"
+  },
+  {
+    "revision": "05557629a9067fc85e52",
+    "url": "/0316_develop/static/js/1.05557629.chunk.js"
+  },
+  {
+    "revision": "82bab410354ef66c452fe3302ff8cc21",
+    "url": "/0316_develop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/0316_develop/precache-manifest.c21de7d1de0518f5bd16e083cda89ed0.js"
+  "/0316_develop/precache-manifest.4d295711e593259f1280106c2d7aa758.js"
 );
 
 workbox.clientsClaim();
